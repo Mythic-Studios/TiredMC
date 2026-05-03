6cf9d5c8f65f3d9f1826034f4a739f6d32881d61
MarsMyth <callumshelton2503@gmail.com>
2026-05-03 16:20:11 +1000

    Initial

---
+++ src/main/java/org/mythic_goose/challenge_series/tiredmc/TiredMCClient.java
@@ -0,0 +1,10 @@
+package org.mythic_goose.challenge_series.tiredmc;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TiredMCClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/org/mythic_goose/challenge_series/tiredmc/TiredMCDataGenerator.java
@@ -0,0 +1,11 @@
+package org.mythic_goose.challenge_series.tiredmc;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TiredMCDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
